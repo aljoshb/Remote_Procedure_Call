c48f8cb634e709c00a87899d6b7f0726a6b8c06a
JK Liu <jk.liu@uwaterloo.ca>
2018-03-01 11:13:02 -0500

    added basic structure for rpcExecute and rpcTerminate

--- rpc.c
+++ rpc.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "rpc.h"
+#include "binder.h"
+
+int fdClientAccept = -1;
+int fdBinder = -1;
 
 int rpcInit() { /*Josh*/
-
+	
 	return 0;
 }
 int rpcCall(char* name, int* argTypes, void** args) { /*Josh*/
@@ -16,11 +20,36 @@ int rpcRegister(char* name, int* argTypes, skeleton f) { /*Josh*/
 	return 0;
 }
 int rpcExecute() { /*Jk*/
+	// main server loop
 	
-	return 0;
+	int messageType = 0;
+
+	// handle termination
+	if (messageType == TERMINATE) {
+		const char *hostname = getenv("BINDER_ADDRESS");
+		const char *portStr = getenv("BINDER_PORT");
+		
+		// check if termination from binder
+		
+
+		// binder requested termination
+		return 0;
+	}
+
+	// forward request to skeleton
+	else if (messageType == EXECUTE) {
+	
+		// return results to client
+	}
+
+
 }
 int rpcTerminate() { /*Jk*/
+	const char *hostname = getenv("BINDER_ADDRESS");
+	const char *portStr = getenv("BINDER_PORT");
+
+	// client passes request to binder
 
 	return 0;
 }
-
+// length/type/message
