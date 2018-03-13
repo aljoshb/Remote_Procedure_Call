721336d36a6959e763d5a3c5a2c4bdc375e5a57f
joshuaalawode <joshalawode@gmail.com>
2018-03-13 18:59:30 -0400

    Updated communication_functions.h with getUniqueFunctionKey

--- communication_functions.h
+++ communication_functions.h
@@ -9,6 +9,7 @@ int receiveInt(int socketfd, uint32_t *numToReceive, size_t length, int flags);
 int sendMessage(int socketfd, char *messageToSend, size_t length, int flags);
 int receiveMessage(int socketfd, char *messageToReceive, size_t length, int flags);
 int connection(const char *destIP, const char *destPort);
+std::string getUniqueFunctionKey(char* funcName, int* argTypes);
 
 #ifdef __cplusplus
 }
